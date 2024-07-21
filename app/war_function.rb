c4da49b20983eeb6ed778e40a5fee5605099973e
haruki1227041209 <haruki122704@gmail.com>
2024-07-21 14:43:47 +0900

    step2完成

--- app/war_function.rb
+++ app/war_function.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "player_cards"
+#require 'debug'
 
 class WarFunction
   attr_accessor :player_cards
@@ -17,28 +18,54 @@ class WarFunction
     @save_player_card = []
     puts "戦争を開始します。"
     puts "カードが配られました。"
-    100.times do |suit_index|
-      @save_player1 = @player1[0] # 手札を保存
-      @save_player2 = @player2[0]
-      p @save_player1, @save_player2
+    until @player1.empty? || @player2.empty?
+      #binding.break
+      @save_player1 = @player1[0].dup # 手札を保存
+      @save_player2 = @player2[0].dup
       result = war_result # 結果を先に計算
-      change_cards # 該当する手札をA,J,Q,Kに変換
+      change_cards(@save_player1, @save_player2) # 該当する手札をA,J,Q,Kに変換
 
       puts "戦争！"
-      #p 0
-      #p @player1, @player2
       puts "プレイヤー1のカードは#{@save_player1[0]}の#{@save_player1[1]}です。"
       puts "プレイヤー2のカードは#{@save_player2[0]}の#{@save_player2[1]}です。" # 手札を表示
       puts result
-      break if @player1.empty? || @player2.empty?
+
+      if @player1.empty?
+        @player1 += @get_player1.shuffle
+        @get_player1 = []
+      end
+      if @player2.empty?
+        @player2 += @get_player2.shuffle
+        @get_player2 = []
+      end
+
+      # break if @player1.empty? || @player2.empty?
+      # break unless @player1[0] == @player2[0]
     end
+
+    puts "--------------------------------"
+    if @player1.length > @player2.length
+      puts "プレイヤー2の手札がなくなりました。"
+      puts "プレイヤー1の手札の枚数は#{@player1.length + @get_player1.length}枚です。プレイヤー2の手札の枚数は0枚です。"
+      puts "プレイヤー1が1位、プレイヤー2が2位です。"
+    else
+      puts "プレイヤー1の手札がなくなりました。"
+      puts "プレイヤー2の手札の枚数は#{@player2.length + @get_player2.length}枚です。プレイヤー1の手札の枚数は0枚です。"
+      puts "プレイヤー2が1位、プレイヤー1が2位です。"
+    end
+    puts "戦争を終了します。"
+
+=begin
     p @player1
     p @player2
+    p @player1.length
+    p @player2.length
     p @save_player_card
     p @get_player1
     p @get_player2
     p @get_player1.length
     p @get_player2.length
+=end
   end
 
   def war_result
@@ -53,7 +80,6 @@ class WarFunction
         @save_player_card = []
         @get_player1 << @player1.shift
         @get_player1 << @player2.shift
-        p @get_player1
         "プレイヤー1が勝ちました。プレイヤー1はカードを#{get_cards}枚もらいました。"
       else
         @get_player1 << @player1.shift
@@ -67,7 +93,6 @@ class WarFunction
         @save_player_card = []
         @get_player2 << @player1.shift
         @get_player2 << @player2.shift
-        p @get_player2
         "プレイヤー2が勝ちました。プレイヤー2はカードを#{get_cards}枚もらいました。"
       else
         @get_player2 << @player1.shift
@@ -77,30 +102,30 @@ class WarFunction
     end
   end
 
-  def change_cards
-    change_ace
-    change_jack
-    change_queen
-    change_king
+  def change_cards(save_player1, save_player2)
+    change_ace(save_player1, save_player2)
+    change_jack(save_player1, save_player2)
+    change_queen(save_player1, save_player2)
+    change_king(save_player1, save_player2)
   end
 
-  def change_ace
-    @save_player1[1] = "A" if @save_player1[1] == 14
-    @save_player2[1] = "A" if @save_player2[1] == 14
+  def change_ace(save_player1, save_player2)
+    save_player1[1] = "A" if save_player1[1] == 14
+    save_player2[1] = "A" if save_player2[1] == 14
   end
 
-  def change_jack
-    @save_player1[1] = "J" if @save_player1[1] == 11
-    @save_player2[1] = "J" if @save_player2[1] == 11
+  def change_jack(save_player1, save_player2)
+    save_player1[1] = "J" if save_player1[1] == 11
+    save_player2[1] = "J" if save_player2[1] == 11
   end
 
-  def change_queen
-    @save_player1[1] = "Q" if @save_player1[1] == 12
-    @save_player2[1] = "Q" if @save_player2[1] == 12
+  def change_queen(save_player1, save_player2)
+    save_player1[1] = "Q" if save_player1[1] == 12
+    save_player2[1] = "Q" if save_player2[1] == 12
   end
 
-  def change_king
-    @save_player1[1] = "K" if @save_player1[1] == 13
-    @save_player2[1] = "K" if @save_player2[1] == 13
+  def change_king(save_player1, save_player2)
+    save_player1[1] = "K" if save_player1[1] == 13
+    save_player2[1] = "K" if save_player2[1] == 13
   end
 end
